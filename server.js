ddc6611509c58d6ac377a2ff002fad07af7cbbfa
Samuel <s.gentile91@gmail.com>
2016-11-20 13:37:17 +0000

    update

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 var express = require("express");
+var path = require("path");
 var app = express();
 
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.get('/', function(request, response) {
-   response.send('coming soon'); 
+});
+
+app.get('/new/:query', function(request, response) {
+   var url = request.params.query;
+   response.send(url);
 });
 
 app.listen('8080');
